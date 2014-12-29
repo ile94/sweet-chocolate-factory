8f96189660365477e9d13f310bbc1f66db104f44
GiorgiaMenegoni <giorgia.menegoni@gmail.com>
2014-12-29 16:15:43 +0100

    Alcuni tentativi di modifica su "materiale"

--- materiale.cpp
+++ materiale.cpp
@@ -1,12 +1,23 @@
-
 #include "materiale.h"
 
+
 Materiale :: Materiale() {
+    map<int, string> m;
 	IDMateriale = 0;
 	tipoMateriale = " ";
+	m.insert(pair<int, string> (IDMateriale, string(" ")));
 }
 		
 Materiale::Materiale(int _IDM, string tm) {
 	IDMateriale = _IDM;
 	tipoMateriale = tm;
+}
+
+//ostream& operator<<(ostream& os, const Materiale&){
+         //if ()}
+
+void test_Materiale(){
+     Materiale m;
+     //m.Materiale();
+     //cout<< m.IDMateriale << endl;
 }
--- materiale.h
+++ materiale.h
@@ -1,20 +1,27 @@
-
 #ifndef __MATERIALE_H__
 #define __MATERIALE_H__
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 
 class Materiale {
 	private:
 		int IDMateriale;
 		string tipoMateriale;
+		map<int, string> m;
+		
+		
 	public:
 		Materiale();
-		Materiale(int _IDM, string tm);
+		Materiale(map<int, string>);
+		//Materiale(int _IDM, string tm);
 		void inserisciTipoMateriale();
+		
+		//friend ostream& operator<<(ostream& os, const Materiale&);
 };
 
+void test_Materiale();
 
 #endif
