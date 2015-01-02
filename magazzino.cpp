a834aec571d99972c6aa71fa31e5b704176af7e2
GiorgiaMenegoni <giorgia.menegoni@gmail.com>
2015-01-02 19:22:23 +0100

    Magazzino

--- magazzino.cpp
+++ magazzino.cpp
@@ -1,12 +1,30 @@
-
 #include "magazzino.h"
+#include "tipoprodotto.h"
 
 Magazzino::Magazzino() {
-	IDProdotto = 0;
+    IDProdotto = " ";
 	quantitaDisponibile = 0;	
 }
 
 Magazzino::Magazzino(int _IDP, int qd) {
 	IDProdotto = _IDP;
 	quantitaDisponibile = qd;	
+}
+
+void Magazzino::quantitàdisponibile(){
+     ifstream file ( "prodotti.csv" );
+    ifstream is;
+    char linea[100];
+    is.open("prodotti.csv", ios::in); 
+    
+    is.getline(linea, 100); //leggere la linea id intestazione
+    string value;
+    while ( file.good() )
+    {
+          getline ( file, value, ',' ); // read a string until next comma: http://www.cplusplus.com/reference/string/getline/
+          //cout << string( value, 1, value.length()-2 ); // display value removing the first and the last character from it
+          =IDProdotto
+    }
+    
+    is.close(); 
 }
--- magazzino.h
+++ magazzino.h
@@ -2,6 +2,8 @@
 #define __MAGAZZINO_H__
 #include <cstdlib>
 #include <iostream>
+#include "tipoprodotto.h"
+#include <string>
 
 
 using namespace std;
@@ -9,12 +11,15 @@ using namespace std;
 
 class Magazzino {
 	private:
-		int IDProdotto;
+		string IDProdotto;
 		int quantitaDisponibile;
+		map <string, string> mm;
 	public:
 		Magazzino();
 		Magazzino(int _IDP, int qd);
-		void inserisciProdotto();
+		
+		void quantitàdisponibile();
+        void inserisciProdotto();
 		void rimuoviProdotto();
 		void inserisciQuantita();
 		void modificaQuantita();
