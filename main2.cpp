7ce54df4abdabdb96ec395b036001eb3b4291e40
GiorgiaMenegoni <giorgia.menegoni@gmail.com>
2015-01-04 10:48:32 +0100

    main

---
+++ main2.cpp
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "addetto.h"
+#include "azienda.h"
+#include "cliente.h"
+#include "magazzino.h"
+#include "materiale.h"
+#include "ordine.h"
+#include "ordinevendita.h"
+#include "persona.h"
+#include "rigaordine.h"
+#include "tipoprodotto.h"
+
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    //test_prodotto();
+    //test_materiale();
+    //start();
+    test_cliente();
+    //test_persona();
+    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
